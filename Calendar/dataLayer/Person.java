03452bc9beea180bee46f549fac3b7ffe95472cf
plisik <patrykkbkak@gmail.com>
2018-04-09 09:40:51 +0200

    basic person implementation

--- Calendar/dataLayer/Person.java
+++ Calendar/dataLayer/Person.java
@@ -1,5 +1,33 @@
 package dataLayer;
 
 public class Person {
+	String name;
+	String surName;
+
+	Person(String _name, String _surName) {
+		name = _name;
+		surName = _surName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	Person() {
+		name = "No Name";
+		surName = "No Surname";
+	}
 
 }
