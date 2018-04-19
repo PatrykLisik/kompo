738724e08cf0f0a7beaaf8afd18c0aa0916487f8
plisik <patrykkbkak@gmail.com>
2018-04-19 20:07:52 +0200

    Javadoc added

--- Calendar/src/dataLayer/DataContext.java
+++ Calendar/src/dataLayer/DataContext.java
@@ -2,9 +2,13 @@
 package dataLayer;
 
 import java.util.HashMap;
-
+/**
+ * Class that contains data for data service 
+ * @author plisik
+ *
+ */
 public class DataContext {
-	HashMap<Integer, Person> Persons = new HashMap<Integer, Person>();
-	HashMap<Integer, Event> Events = new HashMap<Integer, Event>();
+	public HashMap<Integer, Person> Persons = new HashMap<Integer, Person>();
+	public HashMap<Integer, Event> Events = new HashMap<Integer, Event>();
 
 }
