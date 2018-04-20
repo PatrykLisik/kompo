40a21a82ca1d040f84ff479da27c6852faaa8162
plisik <patrykkbkak@gmail.com>
2018-04-20 14:23:54 +0200

    Policies skeleton

---
+++ Calendar/src/logicLayer/SortingAndSearchPolicies.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package logicLayer;
+
+import java.util.Date;
+import java.util.List;
+
+import dataLayer.DataService;
+import dataLayer.Event;
+
+/**
+ * Implementation of sorting and searching algorithms demanded by logic layer 
+ * @author plisik
+ *
+ */
+public class SortingAndSearchPolicies {
+	static public List<Event> eventsBetweenDates(DataService data,Date start,Date end){
+		//TODO implement eventsBetweenDates
+		return null;
+	}
+	static public List<Event> eventsByDate(DataService data){
+		//TODO implement eventsByDate
+		return null;
+	}
+	static public List<Event> eventsByNumberOfParticipants(DataService data){
+		//TODO implement eventsByNumberOfParticipants
+		return null;
+	}
+
+}
