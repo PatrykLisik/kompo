b3152a17a0682a650fb5722f39995974c9ea5519
plisik <patrykkbkak@gmail.com>
2018-04-20 14:24:09 +0200

    Seralziation and serial number

--- Calendar/src/dataLayer/DataContext.java
+++ Calendar/src/dataLayer/DataContext.java
@@ -1,13 +1,19 @@
 
 package dataLayer;
 
+import java.io.Serializable;
 import java.util.HashMap;
 /**
  * Class that contains data for data service 
  * @author plisik
  *
  */
-public class DataContext {
+public class DataContext implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3887628138371244519L;
 	public HashMap<Integer, Person> Persons = new HashMap<Integer, Person>();
 	public HashMap<Integer, Event> Events = new HashMap<Integer, Event>();
 
