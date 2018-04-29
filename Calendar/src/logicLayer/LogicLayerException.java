0c6f1e36ecbcc29c5186d9346696fe8dc64b19b8
plisik <patrykkbkak@gmail.com>
2018-04-29 16:08:01 +0200

    Custom exception for logic layer

---
+++ Calendar/src/logicLayer/LogicLayerException.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package logicLayer;
+
+/**
+ * @author plisik
+ *
+ */
+public class LogicLayerException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3099297478265171564L;
+
+	/**
+	 * 
+	 */
+	public LogicLayerException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public LogicLayerException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public LogicLayerException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public LogicLayerException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public LogicLayerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
