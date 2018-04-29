3e238aac9e0637436c2da13a5f3ab397f2bf5fd3
plisik <patrykkbkak@gmail.com>
2018-04-29 16:04:25 +0200

    User input added

--- Calendar/src/textUserInterface/ConsoleInterfaceElements.java
+++ Calendar/src/textUserInterface/ConsoleInterfaceElements.java
@@ -1,7 +1,12 @@
 package textUserInterface;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
- * Repeatable parts of console interface packed up in one place 
+ * Repeatable parts of console interface packed up in one place
+ * 
  * @author plisik
  *
  */
@@ -18,6 +23,33 @@ public class ConsoleInterfaceElements {
 	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
+	}
+
+	static String getUserInput() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = null;
+		try {
+			s = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return s;
+	}
+
+	static Integer getIntegerFromUser() {
+
+		Integer ans = null;
+		while (true) {
+			String number = getUserInput();
+			try {
+				ans = Integer.parseInt(number);
+			} catch (NumberFormatException e) {
+				System.out.println("Incorect number");
+				continue;
+			}
+			break;
+		}
+		return ans;
 	}
 
 }
