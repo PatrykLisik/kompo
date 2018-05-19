e5d4ce8c6c64139797a52a6bb00bc3c6f697cbab
Patryk Lisik <patrykkbkak@gmail.com>
2018-05-19 13:20:19 +0200

    [Baza danych] skrypt inicjujacy

---
+++ Calendar/SQL_init.sql
@@ -0,0 +1,57 @@
+DROP DATABASE IF EXISTS calendar_data;
+CREATE DATABASE calendar_data;
+
+use calendar_data
+
+CREATE TABLE events(
+    id int,
+    name varchar(30),
+    start date,
+    end date
+
+);
+ALTER TABLE events
+ADD PRIMARY KEY (id);
+
+CREATE TABLE persons(
+    id int,
+    name varchar(20),
+    surname varchar(20)
+);
+
+ALTER TABLE persons
+ADD PRIMARY KEY (id);
+
+CREATE TABLE notifications(
+    id int,
+    description varchar(40),
+    notify_date DATETIME
+);
+
+ALTER TABLE notifications
+ADD PRIMARY KEY (id);
+
+CREATE TABLE event_person(
+    person_id int,
+    event_id int
+);
+
+ALTER TABLE event_person
+ADD FOREIGN KEY (person_id) REFERENCES persons(id);
+ALTER TABLE event_person
+ADD FOREIGN KEY (event_id) REFERENCES events(id);
+
+CREATE TABLE notifications_events(
+    event_id int,
+    notification_id int
+);
+
+ALTER TABLE notifications_events
+ADD FOREIGN KEY (event_id) REFERENCES events(id);
+ALTER TABLE notifications_events
+ADD FOREIGN KEY (notification_id) REFERENCES notifications(id);
+
+DESCRIBE persons;
+DESCRIBE event_person;
+DESCRIBE notifications_events;
+DESCRIBE notifications;
